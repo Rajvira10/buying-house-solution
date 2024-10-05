a9af562b98be5a55b011e63aeadfd4c1388befd5
Rajvira10 <rajvirahmedshuvo10@gmail.com>
2024-10-05 14:31:37 +0600

    minor change

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,6 +10,7 @@ use App\Models\Query;
 use App\Models\OrderTna;
 use Illuminate\Http\Request;
 use App\Imports\OrdersImport;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
 
